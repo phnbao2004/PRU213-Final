f8d83e763ed9af57fe37ec25991a1d911eceec47
fyn044 <phuvdqe180138@fpt.edu.vn>
2026-03-23 07:55:43 +0700

    MenuScene
    .

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject guidePanel;
+    public GameObject playButton;
+    public GameObject guideButton;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void ShowGuide()
+    {
+        guidePanel.SetActive(true);
+
+        // ẩn menu chính
+        playButton.SetActive(false);
+        guideButton.SetActive(false);
+    }
+
+    public void CloseGuide()
+    {
+        guidePanel.SetActive(false);
+
+        // hiện lại menu
+        playButton.SetActive(true);
+        guideButton.SetActive(true);
+    }
+}
